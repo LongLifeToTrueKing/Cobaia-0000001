28d644dca140cf87cffe8d919065836afdbc0aa7
LongLifeToTrueKing <allan.sander@outlook.com>
2024-11-18 22:45:01 -0300

    segundo

--- ex00.py
+++ ex00.py
@@ -3,3 +3,5 @@ if pergunta == "sim" :
     print("Parabens Eduardo, agora faltam apenas 12 dias para voce ganhar a aposta.")
 if pergunta == "nao" :
     print("mentira, pois eu te vi la.")
+else :
+    print("invalido,", "responda so com", "sim", "ou", "nao")
