8230ce7ee0e086a67bc124129e1eb4dc52e0a930
LongLifeToTrueKing <allan.sander@outlook.com>
2024-11-18 22:37:58 -0300

    primeiro

---
+++ ex00.py
@@ -0,0 +1,5 @@
+pergunta = input("Ola, você treinou hoje?")
+if pergunta == "sim" :
+    print("Parabens Eduardo, agora faltam apenas 12 dias para voce ganhar a aposta.")
+if pergunta == "nao" :
+    print("mentira, pois eu te vi la.")
